9914bcbd4a236e5d650ea65811e053b0f7836744
afzal <write1afzal@gmail.com>
2026-05-30 19:05:55 +0530

    update

--- Frontend/src/features/auth/service/auth.api.js
+++ Frontend/src/features/auth/service/auth.api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: window.location.hostname === "localhost"
+      ? "http://localhost:3000"
+      : "https://langchain-gemini-assistant.onrender.com",
+
+
   withCredentials: true,
 })
 
