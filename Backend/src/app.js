134a31fe1eb65e39fd0dbc3cab950b35982d0ad3
afzal <write1afzal@gmail.com>
2026-05-30 18:43:23 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,12 @@ import cors from "cors";
 import path from "path";
 
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 
 const app = express();
@@ -33,7 +39,7 @@ app.use("/api/chats", chatRouter);
 
 
 app.use(express.static("./public"));
-app.use("*name", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "./public/index.html"));
   
 })
